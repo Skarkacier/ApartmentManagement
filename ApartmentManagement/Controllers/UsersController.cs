246c2364f7b70992388efb4f5d7541dea6e75350
Skarkacier <sedakarkacier@gmail.com>
2024-05-26 15:03:19 +0300

    Adding UserService and UsersController

---
+++ ApartmentManagement/Controllers/UsersController.cs
@@ -0,0 +1,75 @@
+﻿using Business.Services;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            try
+            {
+                var userId = await _userService.CreateUser(user.FullName, user.IdentityNum, user.Email, user.Password, user.PhoneNumber, user.CarPlate, user.IsAdmin);
+                return Ok(userId);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var users = await _userService.GetAllUsers();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
+        {
+            try
+            {
+                await _userService.UpdateUser(id, user.FullName, user.IdentityNum, user.Email, user.Password, user.PhoneNumber, user.CarPlate, user.IsAdmin);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                await _userService.DeleteUser(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Business/Services/UserService.cs
@@ -0,0 +1,89 @@
+﻿using DataAccess.Context;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Business.Services
+{
+    public interface IUserService
+    {
+        Task<int> CreateUser(string fullName, string identityNum, string email, string password, string phoneNumber, string carPlate, bool isAdmin);
+        Task<List<User>> GetAllUsers();
+        Task UpdateUser(int userId, string fullName, string identityNum, string email, string password, string phoneNumber, string carPlate, bool isAdmin);
+        Task DeleteUser(int userId);
+    }
+    public class UserService : IUserService
+    {
+        private readonly ManagementContext _context;
+
+        public UserService(ManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateUser(string fullName, string identityNum, string email, string password, string phoneNumber, string carPlate, bool isAdmin)
+        {
+            try
+            {
+                var userIdParam = new Microsoft.Data.SqlClient.SqlParameter("@UserID", System.Data.SqlDbType.Int);
+                userIdParam.Direction = System.Data.ParameterDirection.Output;
+
+                await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[User_Create] @UserID OUT, @FullName, @IdentityNum, @Email, @Password, @PhoneNumber, @CarPlate, @IsAdmin",
+                    userIdParam,
+                    new Microsoft.Data.SqlClient.SqlParameter("@FullName", fullName),
+                    new Microsoft.Data.SqlClient.SqlParameter("@IdentityNum", identityNum),
+                    new Microsoft.Data.SqlClient.SqlParameter("@Email", email),
+                    new Microsoft.Data.SqlClient.SqlParameter("@Password", password),
+                    new Microsoft.Data.SqlClient.SqlParameter("@PhoneNumber", phoneNumber),
+                    new Microsoft.Data.SqlClient.SqlParameter("@CarPlate", carPlate),
+                    new Microsoft.Data.SqlClient.SqlParameter("@IsAdmin", isAdmin));
+
+                return (int)userIdParam.Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            try
+            {
+                return await _context.Users.FromSqlRaw("EXEC [dbo].[User_ReadAll]").ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdateUser(int userId, string fullName, string identityNum, string email, string password, string phoneNumber, string carPlate, bool isAdmin)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[User_Update] {userId}, {fullName}, {identityNum}, {email}, {password}, {phoneNumber}, {carPlate}, {isAdmin}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task DeleteUser(int userId)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[User_Delete] {userId}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
