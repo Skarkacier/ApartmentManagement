ad5d13c4698fe675d66749e26f2a8b08dea2a133
Skarkacier <sedakarkacier@gmail.com>
2024-05-27 18:38:51 +0300

    Adding MessageService and MessagesController

---
+++ ApartmentManagement/Controllers/MessagesController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Services;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage([FromBody] Message message)
+        {
+            try
+            {
+                var messageId = await _messageService.CreateMessage(message.MessageText, message.SenderId, message.ReceiverId, message.IsRead, message.MessageDate);
+                return Ok(messageId);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMessages()
+        {
+            try
+            {
+                var messages = await _messageService.GetAllMessages();
+                return Ok(messages);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMessage(int id, [FromBody] Message message)
+        {
+            try
+            {
+                await _messageService.UpdateMessage(id, message.MessageText, message.SenderId, message.ReceiverId, message.IsRead, message.MessageDate);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            try
+            {
+                await _messageService.DeleteMessage(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Business/Services/MessageService.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Context;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public interface IMessageService
+    {
+        Task<int> CreateMessage(string messageText, int senderId, int receiverId, bool isRead, DateTime messageDate);
+        Task<List<Message>> GetAllMessages();
+        Task UpdateMessage(int messageId, string messageText, int senderId, int receiverId, bool isRead, DateTime messageDate);
+        Task DeleteMessage(int messageId);
+    }
+    public class MessageService : IMessageService
+    {
+        private readonly ManagementContext _context;
+
+        public MessageService(ManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreateMessage(string messageText, int senderId, int receiverId, bool isRead, DateTime messageDate)
+        {
+            try
+            {
+                var messageIdParam = new Microsoft.Data.SqlClient.SqlParameter("@MessageID", System.Data.SqlDbType.Int);
+                messageIdParam.Direction = System.Data.ParameterDirection.Output;
+
+                await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[Message_Create] @MessageID OUT, @MessageText, @SenderId, @ReceiverId, @IsRead, @MessageDate",
+                    messageIdParam,
+                    new Microsoft.Data.SqlClient.SqlParameter("@MessageText", messageText),
+                    new Microsoft.Data.SqlClient.SqlParameter("@SenderId", senderId),
+                    new Microsoft.Data.SqlClient.SqlParameter("@ReceiverId", receiverId),
+                    new Microsoft.Data.SqlClient.SqlParameter("@IsRead", isRead),
+                    new Microsoft.Data.SqlClient.SqlParameter("@MessageDate", messageDate));
+
+                return (int)messageIdParam.Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<Message>> GetAllMessages()
+        {
+            try
+            {
+                return await _context.Messages.FromSqlRaw("EXEC [dbo].[Message_ReadAll]").ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdateMessage(int messageId, string messageText, int senderId, int receiverId, bool isRead, DateTime messageDate)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[Message_Update] {messageId}, {messageText}, {senderId}, {receiverId}, {isRead}, {messageDate}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task DeleteMessage(int messageId)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[Message_Delete] {messageId}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
