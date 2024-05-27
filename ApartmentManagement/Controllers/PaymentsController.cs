102c8179f71a140c1180aa5697540f33c48d48d0
Skarkacier <sedakarkacier@gmail.com>
2024-05-27 18:37:53 +0300

    Adding PaymentService and PaymentsController

---
+++ ApartmentManagement/Controllers/PaymentsController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Services;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePayment([FromBody] Payment payment)
+        {
+            try
+            {
+                var paymentId = await _paymentService.CreatePayment(payment.Type, payment.Amount, payment.ApartmentID, payment.UserID, payment.IsPayed, payment.PaymentDate);
+                return Ok(paymentId);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPayments()
+        {
+            try
+            {
+                var payments = await _paymentService.GetAllPayments();
+                return Ok(payments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePayment(int id, [FromBody] Payment payment)
+        {
+            try
+            {
+                await _paymentService.UpdatePayment(id, payment.Type, payment.Amount, payment.ApartmentID, payment.UserID, payment.IsPayed, payment.PaymentDate);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePayment(int id)
+        {
+            try
+            {
+                await _paymentService.DeletePayment(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Business/Services/PaymentService.cs
@@ -0,0 +1,86 @@
+﻿using DataAccess.Context;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public interface IPaymentService
+    {
+        Task<int> CreatePayment(string type, decimal amount, int apartmentId, int userId, bool isPayed, DateTime paymentDate);
+        Task<List<Payment>> GetAllPayments();
+        Task UpdatePayment(int paymentId, string type, decimal amount, int apartmentId, int userId, bool isPayed, DateTime paymentDate);
+        Task DeletePayment(int paymentId);
+    }
+    public class PaymentService : IPaymentService
+    {
+        private readonly ManagementContext _context;
+
+        public PaymentService(ManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CreatePayment(string type, decimal amount, int apartmentId, int userId, bool isPayed, DateTime paymentDate)
+        {
+            try
+            {
+                var paymentIdParam = new Microsoft.Data.SqlClient.SqlParameter("@PaymentID", System.Data.SqlDbType.Int);
+                paymentIdParam.Direction = System.Data.ParameterDirection.Output;
+
+                await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[Payment_Create] @PaymentID OUT, @Type, @Amount, @ApartmentId, @UserId, @IsPayed, @PaymentDate",
+                    paymentIdParam,
+                    new Microsoft.Data.SqlClient.SqlParameter("@Type", type),
+                    new Microsoft.Data.SqlClient.SqlParameter("@Amount", amount),
+                    new Microsoft.Data.SqlClient.SqlParameter("@ApartmentId", apartmentId),
+                    new Microsoft.Data.SqlClient.SqlParameter("@UserId", userId),
+                    new Microsoft.Data.SqlClient.SqlParameter("@IsPayed", isPayed),
+                    new Microsoft.Data.SqlClient.SqlParameter("@PaymentDate", paymentDate));
+
+                return (int)paymentIdParam.Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<Payment>> GetAllPayments()
+        {
+            try
+            {
+                return await _context.Payments.FromSqlRaw("EXEC [dbo].[Payment_ReadAll]").ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdatePayment(int paymentId, string type, decimal amount, int apartmentId, int userId, bool isPayed, DateTime paymentDate)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[Payment_Update] {paymentId}, {type}, {amount}, {apartmentId}, {userId}, {isPayed}, {paymentDate}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task DeletePayment(int paymentId)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync($"EXEC [dbo].[Payment_Delete] {paymentId}");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
